80bf38dc46f2f457d8f5b16c836d7c8982e31f0e
Carlos Eduardo Monti <slaytanic@gmail.com>
2024-07-22 11:26:13 +0200

    Add test

--- src/PokemonSearch.test.tsx
+++ src/PokemonSearch.test.tsx
@@ -3,6 +3,7 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { PokemonSearch } from "./PokemonSearch";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { getPokemon } from "./api";
@@ -22,10 +23,23 @@ describe("PokemonSearch", () => {
   it("fetches and renders the default Pokemon", async () => {
     vi.mocked(getPokemon).mockResolvedValue({
       status: "ok",
-      pokemon: { name: "pikachu", order: 1 },
+      pokemon: { name: "ditto", order: 1 },
     });
     setupComponent();
     await waitForElementToBeRemoved(screen.getByText("Loading..."));
+    expect(screen.getByText("ditto")).toBeInTheDocument();
+  });
+
+  it("fetches and renders the default Pokemon", async () => {
+    vi.mocked(getPokemon).mockResolvedValue({
+      status: "ok",
+      pokemon: { name: "pikachu", order: 2 },
+    });
+    setupComponent();
+    const textInput = screen.getByLabelText("Pokemon name:");
+    await userEvent.clear(textInput);
+    await userEvent.type(textInput, "pikachu");
+    expect(getPokemon).toHaveBeenCalledWith("pikachu");
     expect(screen.getByText("pikachu")).toBeInTheDocument();
   });
 });
